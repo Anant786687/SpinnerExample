5414dd0e02e07d510421a7da34d8aac66b411def
Anant <anantkmr67@gmail.com>
2019-02-05 19:38:54 +0530

    Initial commit

---
+++ app/src/main/java/com/andro/anant/spinnerexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andro.anant.spinnerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
